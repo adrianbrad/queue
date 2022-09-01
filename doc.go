9d0f07b0ee62d2ae6661b507607886604c2fa791
adrianbrad <me@adrianbrad.com>
2022-09-01 15:43:56 +0300

    docs(package_comment): add package comment

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Package queue provides different thread-safe generic queue implementations.
+//
+// The blocking waits for the queue have elements available
+// before retrieving from it.
+// The blocking queue can be reset in order to make all elements
+// available again.
+package queue
